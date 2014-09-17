81d3f3139250c0da595e35607886d3e474cb289e
Zack <iam@zackpollard.pro>
2014-09-17 03:31:37 +0100

    Setup ImageUploadManager for holding the map of uploaders against their selected names.

--- src/main/java/pro/zackpollard/projectx/managers/ImageUploadManager.java
+++ src/main/java/pro/zackpollard/projectx/managers/ImageUploadManager.java
@@ -1,17 +1,29 @@
 package pro.zackpollard.projectx.managers;
 
 import pro.zackpollard.projectx.ProjectX;
+import pro.zackpollard.projectx.uploaders.image.CustomImageUploader;
 import pro.zackpollard.projectx.uploaders.image.ImageUploader;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class ImageUploadManager {
 
     private ProjectX instance;
     private ImageUploader selectedUploader;
 
+	private Map<String, ImageUploader> uploaderMap = new HashMap<String, ImageUploader>();
+
     public ImageUploadManager(ProjectX instance) {
 
         this.instance = instance;
+	    this.initiateUploaders();
         //TODO: Get selected uploader from config.
+
+	    //TODO: Remove this once the GUI or there is ability to change this through CLI.
+	    this.setUploader(uploaderMap.get("Custom"));
     }
 
     public ImageUploader getUploader() {
@@ -29,4 +41,23 @@ public class ImageUploadManager {
         this.selectedUploader = uploader;
         //TODO: Save new uploader back to config
     }
+
+
+	/**
+	 * When a new ImageUploader is created, it should be added here with the name that it should have in the GUI. This makes it available for selection.
+	 */
+	private void initiateUploaders() {
+
+		new CustomImageUploader(instance, "Custom");
+	}
+
+	public void addImageUploader(String name, ImageUploader uploader) {
+
+		this.uploaderMap.put(name, uploader);
+	}
+
+	public Map<String, ImageUploader> getImageUploaders() {
+
+		return this.uploaderMap;
+	}
 }
