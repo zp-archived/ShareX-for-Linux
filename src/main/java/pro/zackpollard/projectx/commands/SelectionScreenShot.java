33d5d12f95038559fcb7a7055857aeb2940b77e2
Zack <iam@zackpollard.pro>
2014-09-17 03:46:20 +0100

    Change SelectionScreenShot constructor to included ProjectX variable as required by Command.

--- src/main/java/pro/zackpollard/projectx/commands/SelectionScreenShot.java
+++ src/main/java/pro/zackpollard/projectx/commands/SelectionScreenShot.java
@@ -1,9 +1,11 @@
 package pro.zackpollard.projectx.commands;
 
 
+import pro.zackpollard.projectx.ProjectX;
+
 public class SelectionScreenShot extends Command {
-    public SelectionScreenShot(String name) {
-        super(name);
+    public SelectionScreenShot(ProjectX projectX, String name) {
+        super(projectX, name);
     }
 
     @Override
