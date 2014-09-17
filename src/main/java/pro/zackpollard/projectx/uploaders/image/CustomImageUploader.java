dc75675cd127a3fdf238db8ec87e1da7d9973b37
Zack <iam@zackpollard.pro>
2014-09-17 03:33:32 +0100

    Added a CustomImageUploader with only default code currently.

---
+++ src/main/java/pro/zackpollard/projectx/uploaders/image/CustomImageUploader.java
@@ -0,0 +1,36 @@
+package pro.zackpollard.projectx.uploaders.image;
+
+import pro.zackpollard.projectx.ProjectX;
+import pro.zackpollard.projectx.uploaders.UploadStatus;
+
+/**
+ * Created by Zack on 17/09/2014.
+ */
+public class CustomImageUploader extends ImageUploader {
+
+
+	public CustomImageUploader(ProjectX projectX, String name) {
+		super(projectX, name);
+	}
+
+	@Override
+	public UploadStatus testConnection() {
+
+		//TODO: This runs a test upload and checks if the image uploaded successfully.
+
+		return null;
+	}
+
+	@Override
+	public boolean isSetup() {
+
+		//TODO: This needs to check if the config contains settings for this uploader.
+
+		return false;
+	}
+
+	@Override
+	public void runSetup() {
+		//TODO: Once GUI is implemented do a setup run if this is enabled.
+	}
+}
