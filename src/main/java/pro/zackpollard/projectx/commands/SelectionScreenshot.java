e5fcfd47e2c5cceb3f03d0a41384c6cc57dbbfe1
Zack <zackpollard@ymail.com>
2014-09-18 03:10:17 +0100

    Time to run some tests based on code from http://stackoverflow.com/questions/13948122/drawing-a-bounding-rectangle-to-select-what-area-to-record

--- src/main/java/pro/zackpollard/projectx/commands/SelectionScreenshot.java
+++ src/main/java/pro/zackpollard/projectx/commands/SelectionScreenshot.java
@@ -3,12 +3,10 @@ package pro.zackpollard.projectx.commands;
 import pro.zackpollard.projectx.ProjectX;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.image.BufferedImage;
+import java.awt.event.*;
+import java.awt.geom.Area;
 
 /**
  * Created by Zack on 18/09/2014.
@@ -22,78 +20,165 @@ public class SelectionScreenshot extends Command {
 
     @Override
     public void execute() {
-        final Dimension screenSize = Toolkit.getDefaultToolkit().
-                getScreenSize();
-        final BufferedImage screen = ProjectX.getRobot().createScreenCapture(
-                new Rectangle(screenSize));
-        this.loadScreen(screen);
-    }
-
-    Rectangle captureRect;
-
-    public void loadScreen(final BufferedImage screen) {
-
-        final BufferedImage screenCopy = new BufferedImage(
-                screen.getWidth(),
-                screen.getHeight(),
-                screen.getType());
-        final JLabel screenLabel = new JLabel(new ImageIcon(screenCopy));
-        //JScrollPane screenScroll = new JScrollPane(screenLabel);
-
-        /*screenScroll.setPreferredSize(new Dimension(
-                (int) (screen.getWidth()),
-                (int) (screen.getHeight())));
-        */
-        screenLabel.setBorder(new EmptyBorder(0, 0, 0, 0));
-        JPanel panel = new JPanel(new CardLayout(0, 0));
-        panel.setBorder(new EmptyBorder(0, 0, 0, 0));
-        panel.add(screenLabel);
-
-        /*final JLabel selectionLabel = new JLabel(
-                "Drag a rectangle in the screen shot!");
-        panel.add(selectionLabel, BorderLayout.SOUTH);
-        */
-        repaint(screen, screenCopy);
-        screenLabel.repaint();
-        panel.setVisible(true);
-
-        screenLabel.addMouseMotionListener(new MouseMotionAdapter() {
-
-            Point start = new Point();
-
+        EventQueue.invokeLater(new Runnable() {
             @Override
-            public void mouseMoved(MouseEvent me) {
-                start = me.getPoint();
-                repaint(screen, screenCopy);
-                //selectionLabel.setText("Start Point: " + start);
-                screenLabel.repaint();
-            }
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                }
 
-            @Override
-            public void mouseDragged(MouseEvent me) {
-                Point end = me.getPoint();
-                captureRect = new Rectangle(start,
-                        new Dimension(end.x - start.x, end.y - start.y));
-                repaint(screen, screenCopy);
-                screenLabel.repaint();
-                //selectionLabel.setText("Rectangle: " + captureRect);
+                JFrame frame = new JFrame();
+                frame.setUndecorated(true);
+                // This works differently under Java 6
+                frame.setBackground(new Color(0, 0, 0, 0));
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new BorderLayout());
+                frame.add(new SnipItPane());
+                frame.setBounds(getVirtualBounds());
+                frame.setVisible(true);
             }
         });
-
-        //JOptionPane.showMessageDialog(null, panel);
-
-        System.out.println("Rectangle of interest: " + captureRect);
     }
 
-    public void repaint(BufferedImage orig, BufferedImage copy) {
-        Graphics2D g = copy.createGraphics();
-        g.drawImage(orig, 0, 0, null);
-        if (captureRect != null) {
-            g.setColor(Color.RED);
-            g.draw(captureRect);
-            g.setColor(new Color(255, 255, 255, 150));
-            g.fill(captureRect);
+    public class SnipItPane extends JPanel {
+
+        private Point mouseAnchor;
+        private Point dragPoint;
+
+        private SelectionPane selectionPane;
+
+        public SnipItPane() {
+            setOpaque(false);
+            setLayout(null);
+            selectionPane = new SelectionPane();
+            add(selectionPane);
+            MouseAdapter adapter = new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    mouseAnchor = e.getPoint();
+                    dragPoint = null;
+                    selectionPane.setLocation(mouseAnchor);
+                    selectionPane.setSize(0, 0);
+                }
+
+                @Override
+                public void mouseDragged(MouseEvent e) {
+                    dragPoint = e.getPoint();
+                    int width = dragPoint.x - mouseAnchor.x;
+                    int height = dragPoint.y - mouseAnchor.y;
+
+                    int x = mouseAnchor.x;
+                    int y = mouseAnchor.y;
+
+                    if (width < 0) {
+                        x = dragPoint.x;
+                        width *= -1;
+                    }
+                    if (height < 0) {
+                        y = dragPoint.y;
+                        height *= -1;
+                    }
+                    selectionPane.setBounds(x, y, width, height);
+                    selectionPane.revalidate();
+                    repaint();
+                }
+            };
+            addMouseListener(adapter);
+            addMouseMotionListener(adapter);
         }
-        g.dispose();
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            Graphics2D g2d = (Graphics2D) g.create();
+
+            Rectangle bounds = new Rectangle(0, 0, getWidth(), getHeight());
+            Area area = new Area(bounds);
+            area.subtract(new Area(selectionPane.getBounds()));
+
+            g2d.setColor(new Color(192, 192, 192, 64));
+            g2d.fill(area);
+
+        }
+    }
+
+    public class SelectionPane extends JPanel {
+
+        private JButton button;
+        private JLabel label;
+
+        public SelectionPane() {
+            button = new JButton("Close");
+            setOpaque(false);
+
+            label = new JLabel("Rectangle");
+            label.setOpaque(true);
+            label.setBorder(new EmptyBorder(4, 4, 4, 4));
+            label.setBackground(Color.GRAY);
+            label.setForeground(Color.WHITE);
+            setLayout(new GridBagLayout());
+
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            add(label, gbc);
+
+            gbc.gridy++;
+            add(button, gbc);
+
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    SwingUtilities.getWindowAncestor(SelectionPane.this).dispose();
+                }
+            });
+
+            addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    label.setText("Rectangle " + getX() + "x" + getY() + "x" + getWidth() + "x" + getHeight());
+                }
+            });
+
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g.create();
+            // I've chosen NOT to fill this selection rectangle, so that
+            // it now appears as if you're "cutting" away the selection
+//            g2d.setColor(new Color(128, 128, 128, 64));
+//            g2d.fillRect(0, 0, getWidth(), getHeight());
+
+            float dash1[] = {10.0f};
+            BasicStroke dashed =
+                    new BasicStroke(3.0f,
+                            BasicStroke.CAP_BUTT,
+                            BasicStroke.JOIN_MITER,
+                            10.0f, dash1, 0.0f);
+            g2d.setColor(Color.BLACK);
+            g2d.setStroke(dashed);
+            g2d.drawRect(0, 0, getWidth() - 3, getHeight() - 3);
+            g2d.dispose();
+        }
+    }
+
+    public static Rectangle getVirtualBounds() {
+
+        Rectangle bounds = new Rectangle(0, 0, 0, 0);
+
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice lstGDs[] = ge.getScreenDevices();
+        for (GraphicsDevice gd : lstGDs) {
+
+            bounds.add(gd.getDefaultConfiguration().getBounds());
+
+        }
+
+        return bounds;
+
     }
 }
