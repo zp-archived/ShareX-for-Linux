56a4dc54c4aeab742a33e68c3af308cc8e75bf85
Zack Pollard <zackpollard@ymail.com>
2014-09-22 16:15:46 +0100

    Added getOS method to Utils class for special cases where certain parts of the program do not work, E.G. Robot screenshotting on Unix systems.

--- src/main/java/pro/zackpollard/projectx/utils/Utils.java
+++ src/main/java/pro/zackpollard/projectx/utils/Utils.java
@@ -9,8 +9,8 @@ public class Utils {
      * This generates a file name based on the data and time of generation.
      * It also appends the provided suffix to the generated fileName.
      *
-     * @param suffix This is the String of text to be appended to the generated fileName.
-     * @return The entire generated string, suffix included.
+     * @param suffix This is the String of text to be appended to the generated fileName
+     * @return The entire generated string, suffix included
      */
 
     public static String generateFileName(String suffix) {
@@ -19,5 +19,36 @@ public class Utils {
         Date date = new Date();
         String fileName = sdfDate.format(date) + "." + suffix;
         return fileName;
+    }
+
+    /**
+     * This is used to find out the type of operating system that the program
+     * is running on in order to specialise some methods that don't work on certain
+     * operating systems.
+     *
+     * @return OSType which specifies the operating system type otherwise returns OSType.OTHER
+     * @see OSType
+     */
+
+    public static OSType getOS() {
+
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.startsWith("win")) {
+
+            return OSType.WINDOWS;
+        } else if (os.startsWith("mac")) {
+
+            return OSType.MAC;
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+
+            return OSType.UNIX;
+        } else if (os.startsWith("sunos")) {
+
+            return OSType.SOLARIS;
+        } else {
+
+            return OSType.OTHER;
+        }
     }
 }
